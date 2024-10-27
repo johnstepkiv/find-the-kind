b5d4eda94c92a6959c49050b2923086ae42ffba3
Serhii <serhii.poklonskyi@outlook.de>
2024-10-27 19:13:29 +0100

    created data asset

---
+++ Source/find_the_kind/Public/FindTheKindDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "FindTheKindDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIND_THE_KIND_API UFindTheKindDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
